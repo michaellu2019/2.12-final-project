34f0957bd054c8dff5bd4df4eea9d8b1dbbc506a
Michael Lu <mlu0708@mit.edu>
2022-04-18 08:10:39 +0000

    Center detection?

--- src/isolate_brick.py
+++ src/isolate_brick.py
@@ -32,13 +32,16 @@ def load_img_from_folder(folder_path):
     return pictures
 
 #This function removes isolated pixels
-def denoise_img(image):
+def denoise_img(image, num_steps):
     kernel = np.ones((3,3), np.uint8)
     #Step one erode
     er = cv2.erode(image, kernel, iterations=1)
     #Step 2 dilate and fill missing values we removed
     dil = cv2.dilate(er, kernel, iterations=1)
-    return dil
+    
+    dil2 = cv2.dilate(dil, kernel, iterations=1)
+    er2 = cv2.erode(dil2, kernel, iterations=1)
+    return er2
 
 #This function returns the hue color range for specific brick
 def brick_range(state):
@@ -55,10 +58,10 @@ def brick_range(state):
 
 file_path = './brick_pictures' ; # folder to read photos from
 images = load_img_from_folder(file_path)
-cur_img = images[8]
+cur_img = images[10]
 hsv = cv2.cvtColor(cur_img, cv2.COLOR_BGR2HSV) # converts photo from RGB to HSV
-cv2.namedWindow('Color_Frame')
-cv2.namedWindow('Hue_Frame')
+# cv2.namedWindow('Color_Frame')
+# cv2.namedWindow('Hue_Frame')
 
 #TODO: delete this comment section. This is logic to isolate single color
 
@@ -89,11 +92,11 @@ while True:
             threshold_1 = cv2.inRange(hsv, lower_range_1, upper_range_1 )
             threshold_2 = cv2.inRange(hsv, lower_range_2, upper_range_2 )
             threshold = cv2.bitwise_or(threshold_1, threshold_2, mask=None) # combine both threshold images
-            clean_mask = denoise_img(threshold)
+            clean_mask = denoise_img(threshold, 5)
         else:
             lower_range, upper_range, color_state = brick_range(i)
             threshold = cv2.inRange(hsv, lower_range, upper_range ) #Capture threshold of brick within set color range
-            clean_mask = denoise_img(threshold)
+            clean_mask = denoise_img(threshold, 5)
 
         area = np.count_nonzero(clean_mask)
         print('Current area is: ' + str(area))
@@ -101,9 +104,29 @@ while True:
             #Count total number of white pixels in mask. If its above our area limit we found brick
             print('We found a brick!')
             print('Area is: ' + str(area))
-            cv2.imshow('Hue_Frame', clean_mask)
-            cv2.imshow('Before_denoise', threshold)
+            # cv2.imshow('Hue_Frame', clean_mask)
+            # cv2.imshow('Before_denoise', threshold)
             #This is where we would now update status and move to grabbing mode
+            
+            img, contours, hierarchy = cv2.findContours(clean_mask, 1, 2)
+            
+            # cnt = contours[0]
+            M = cv2.moments(img)
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            print(cx, cy)
+            
+            # rect = cv2.minAreaRect(hierarchy)
+            # box = cv2.boxPoints(rect)
+            # box = np.int0(box)
+            # cv2.drawContours(cur_img,[box], 0, (0,0,255), 2)
+
+            # x,y,w,h = cv2.boundingRect(cnt)
+            # cv2.rectangle(cur_img,(x,y),(x+w,y+h),(0,255,0),2)
+
+            cv2.circle(cur_img, (cx, cy), 10, (255, 0, 0), -1)
+            cv2.imshow("Center_Detection", cur_img)
+
         else:
             print('NO Brick of color: {} found'.format(color_state))
         
